1697508dfaf11f7a973b8efe216d25a1317704aa
이동규 <brainbackdoor@gmail.com>
2023-10-05 06:08:49 +0900

    feat: analysis controller에 관리자 권한 부여

--- analysis/src/main/kotlin/com/brainbackdoor/analysis/ui/AnalysisController.kt
+++ analysis/src/main/kotlin/com/brainbackdoor/analysis/ui/AnalysisController.kt
@@ -1,6 +1,7 @@
 package com.brainbackdoor.analysis.ui
 
 import com.brainbackdoor.analysis.application.AnalysisService
+import com.brainbackdoor.auth.AdminAuth
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,20 +15,24 @@ class AnalysisController(
 ) {
 
     @GetMapping("/coding-as-hobby")
+    @AdminAuth
     fun findCodingAsHobby(): ResponseEntity<List<CodingAsHobbyResponse>> =
         ResponseEntity.ok(analysisService.findCodingAsHobby())
 
     @GetMapping("/lectures-by-participants")
+    @AdminAuth
     fun findLecturesByParticipants(): ResponseEntity<List<LecturesByParticipantsResponse>> =
         ResponseEntity.ok(analysisService.findLecturesByParticipants())
 
     @GetMapping("/lecture-name")
+    @AdminAuth
     fun findLectureNameOrderSurveyId(
         @RequestParam id: Long = 0L,
     ): ResponseEntity<List<LectureNameOrderSurveyIdResponse>> =
         ResponseEntity.ok(analysisService.findLectureNameOrderSurveyId(id))
 
     @GetMapping("member-by-infra-workshop")
+    @AdminAuth
     fun findMemberByInfraWorkshop(): ResponseEntity<List<MemberByInfraWorkshopResponse>> =
         ResponseEntity.ok(analysisService.findMemberByInfraWorkshop())
 }
