6e66d19d6e2e8fce7b1eb51d86e51a4634aaa35b
Herb <greeng00se@kakao.com>
2023-12-31 01:05:56 +0900

    이벤트 컨트롤러에 `@RestController` 애너테이션이 누락된 부분 수정 (#1)
    * feat(conference): evnet controller에 RestController 애너테이션 추가
    
    * feat(front): evnet controller에 RestController 애너테이션 추가

--- conference/src/main/kotlin/com/brainbackdoor/events/EventController.kt
+++ conference/src/main/kotlin/com/brainbackdoor/events/EventController.kt
@@ -7,10 +7,12 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 
 @Tag(name = "EventController", description = "이벤트시스템 API")
 @RequestMapping("/api/events")
+@RestController
 class EventController(
     private val eventService: EventService,
 ) {
--- front/src/main/kotlin/com/brainbackdoor/events/EventController.kt
+++ front/src/main/kotlin/com/brainbackdoor/events/EventController.kt
@@ -7,9 +7,11 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @Tag(name = "EventController", description = "BFF 이벤트 API")
 @RequestMapping("/api/events")
+@RestController
 class EventController(
     private val eventClient: EventClient
 ) {
